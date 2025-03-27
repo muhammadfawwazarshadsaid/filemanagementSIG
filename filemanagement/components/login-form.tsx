f51ef3d6930182b5d148b837f9193d9328f2b742
Muhammad Fawwaz Arshad Said <muhammad.fawwaz22@ui.ac.id>
2025-03-27 22:13:40 +0700

    feat: [Front End] fix login button

--- filemanagement/components/login-form.tsx
+++ filemanagement/components/login-form.tsx
@@ -65,7 +65,7 @@ export function LoginForm({
             </button>
           </div>
         </div>
-        <Button type="submit" className="text-white w-full text-bold">
+        <Button type="submit" className="w-full font-bold">
           Login
         </Button>
         <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
